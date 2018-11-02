3b947460f523b2a6b80a492c0d24b4869cdcc405
Mark <mbergmandy@gmail.com>
2018-11-02 16:32:38 -0500

    create users table and model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+# Controller for Articles
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
@@ -15,7 +16,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:success] = "Article was successfully created"
+      flash[:success] = 'Article was successfully created'
       redirect_to article_path(@article)
     else
       render 'new'
@@ -24,19 +25,19 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:success] = "Article was successfully updated"
+      flash[:success] = 'Article was successfully updated'
       redirect_to article_path(@article)
     else
       render 'edit'
     end
   end
 
-  def show
+  def show 
   end
 
   def destroy
     @article.destroy
-    flash[:danger] = "Article was successfully deleted"
+    flash[:danger] = 'Article was successfully deleted'
     redirect_to articles_path
   end
 
@@ -44,6 +45,7 @@ class ArticlesController < ApplicationController
   def set_article
     @article = Article.find(params[:id])
   end
+
   def article_params
     params.require(:article).permit(:title, :description)
   end
